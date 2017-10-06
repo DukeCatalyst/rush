2becf19b76829f02d09b3573d563fce8ca575c20
Brian Li <brian.li1@duke.edu>
2017-10-06 17:29:41 -0400

    created Xcode project

---
+++ RushApp/RushApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RushApp
+//
+//  Created by Brian Li on 10/6/17.
+//  Copyright © 2017 rush. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
